f0d1fc875e0f08c412301a6d2240ebeb97baa877
Richard Bidin <richardbidin@outlook.com>
2024-01-25 17:41:39 -0300

    Avoids database logging on CI

--- src/infra/databases/postgres/migrator.ts
+++ src/infra/databases/postgres/migrator.ts
@@ -1,16 +1,19 @@
 import path from 'path';
 import Postgrator from 'postgrator';
-import postgres from 'postgres';
+import pg from 'postgres';
 
 import { envs } from 'src/infra/config/envs';
+
+const noop = () => {};
 
 const MIGRATION_MAX_CONNECTIONS = 1;
 const MIGRATION_IDLE_TIMEOUT_IN_SECONDS = 60;
 
 const databaseUrl = `${envs.databaseUrl}/${envs.databaseName}`;
-const migrationConnection = postgres(databaseUrl, {
+const migrationConnection = pg(databaseUrl, {
 	max: MIGRATION_MAX_CONNECTIONS,
 	idle_timeout: MIGRATION_IDLE_TIMEOUT_IN_SECONDS,
+	onnotice: noop,
 });
 
 export const migrator = new Postgrator({
