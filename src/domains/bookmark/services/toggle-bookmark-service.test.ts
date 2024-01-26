7a92ca46afe0d0e33a11cc666ce69a02bcd9bf72
Richard Bidin <richardbidin@outlook.com>
2024-01-25 23:24:07 -0300

    Improves toggle bookmark service error handling

--- src/domains/bookmark/services/toggle-bookmark-service.test.ts
+++ src/domains/bookmark/services/toggle-bookmark-service.test.ts
@@ -3,6 +3,7 @@ import { describe, expect, it } from 'vitest';
 
 import { ToggleBookmarkService } from 'src/domains/bookmark/services/toggle-bookmark-service';
 import { ArticleNotFoundException } from 'src/exceptions/article-not-found-exception';
+import { UserDoesNotExistException } from 'src/exceptions/user-does-not-exist-exception';
 import { ArticlePostgresRepository } from 'src/infra/databases/postgres/repositories/article-postgres-repository';
 import { BookmarkPostgresRepository } from 'src/infra/databases/postgres/repositories/bookmark-postgres-repository';
 import { UserPostgresRepository } from 'src/infra/databases/postgres/repositories/user-postgres-repository';
@@ -13,7 +14,7 @@ const articleRepository = new ArticlePostgresRepository();
 
 describe('ToggleBookmarkSerivce', () => {
 	it('Should return true if an article is bookmarked for the first time', async () => {
-		const toggleBookmarkSerivce = new ToggleBookmarkService(bookmarkRepository, articleRepository);
+		const toggleBookmarkSerivce = new ToggleBookmarkService(bookmarkRepository, articleRepository, userRepository);
 		const user = await userRepository.createOne({ username: 'johndoe', password: 'qwe123', fullName: 'John Doe' });
 		const article = await articleRepository.createOne({ title: 'titulo', content: 'content', userId: user.id });
 
@@ -24,7 +25,7 @@ describe('ToggleBookmarkSerivce', () => {
 	});
 
 	it('Should return false if an article was already bookmarked', async () => {
-		const toggleBookmarkSerivce = new ToggleBookmarkService(bookmarkRepository, articleRepository);
+		const toggleBookmarkSerivce = new ToggleBookmarkService(bookmarkRepository, articleRepository, userRepository);
 		const user = await userRepository.createOne({ username: 'johndoe', password: 'qwe123', fullName: 'John Doe' });
 		const article = await articleRepository.createOne({ title: 'titulo', content: 'content', userId: user.id });
 		await toggleBookmarkSerivce.execute(article.id, user.id);
@@ -36,11 +37,19 @@ describe('ToggleBookmarkSerivce', () => {
 	});
 
 	it('Should throw exception if article does not exist', async () => {
-		const toggleBookmarkSerivce = new ToggleBookmarkService(bookmarkRepository, articleRepository);
+		const toggleBookmarkSerivce = new ToggleBookmarkService(bookmarkRepository, articleRepository, userRepository);
 		const user = await userRepository.createOne({ username: 'johndoe', password: 'qwe123', fullName: 'John Doe' });
 
 		await expect(toggleBookmarkSerivce.execute(randomUUID(), user.id)).rejects.toMatchObject(
 			new ArticleNotFoundException()
+		);
+	});
+
+	it('Should throw exception if user does not exist', async () => {
+		const toggleBookmarkSerivce = new ToggleBookmarkService(bookmarkRepository, articleRepository, userRepository);
+
+		await expect(toggleBookmarkSerivce.execute(randomUUID(), randomUUID())).rejects.toMatchObject(
+			new UserDoesNotExistException()
 		);
 	});
 });
--- src/domains/bookmark/services/toggle-bookmark-service.ts
+++ src/domains/bookmark/services/toggle-bookmark-service.ts
@@ -1,15 +1,25 @@
 import { ArticleRepository } from 'src/domains/article/repositories/article-repository';
 import { BookmarkRepository } from 'src/domains/bookmark/repositories/bookmark-repository';
+import { UserRepository } from 'src/domains/user/repositories/user-repository';
 import { ArticleNotFoundException } from 'src/exceptions/article-not-found-exception';
+import { UserDoesNotExistException } from 'src/exceptions/user-does-not-exist-exception';
 
 export class ToggleBookmarkService {
 	constructor(
 		private readonly bookmarkRepository: BookmarkRepository,
-		private readonly articleRepository: ArticleRepository
+		private readonly articleRepository: ArticleRepository,
+		private readonly userRepository: UserRepository
 	) {}
 
 	async execute(articleId: string, userId: string): Promise<boolean> {
+		const existingUser = await this.userRepository.getOneById(userId);
+
+		if (!existingUser) {
+			throw new UserDoesNotExistException();
+		}
+
 		const existingArticle = await this.articleRepository.getOneById(articleId);
+
 		if (!existingArticle) {
 			throw new ArticleNotFoundException();
 		}
